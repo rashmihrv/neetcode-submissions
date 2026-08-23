f31bf20d4c6771280e33327bf0b532e16b1395ce
rashmihrv <rashmi.hrv@gmail.com>
2026-08-23 13:49:18 +0530

    Bulk sync: 2 submissions

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,10 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        if sorted(s) != sorted(t):
-            return False
+        if sorted(s) == sorted(t):
+            return True
 
-        return True
-        
-        
+        return False
         
